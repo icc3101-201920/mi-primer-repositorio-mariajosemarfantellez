ad1b7c86a753bc3e72675ba9b3eac160b2bda7c9
mariajosemarfantellez <mjmarfan@miuandes.cl>
2020-03-16 15:58:11 -0300

    Guerreros

---
+++ Guerreros/Guerreros/Magos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guerreros
+{
+    public class Combatientes 
+    {
+        public string Atacar;
+
+        public string AtaqueEspecial;
+
+        public string Defender;
+    }
+    public class Armas
+    {
+
+    }
+}
---
+++ Guerreros/Guerreros/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guerreros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Combatientes Mage = new Combatientes();
+            Mage.Atacar = "Atacar";
+            Mage.AtaqueEspecial = "Mage Attack";
+            Mage.Defender = "Defender";
+
+            Combatientes Warrior = new Combatientes();
+            Warrior.Atacar = "Atacar";
+            Warrior.AtaqueEspecial = "Warrior Attack";
+            Warrior.Defender = "Defender";
+        }
+    }
+}
