8f6d2eca97f71b8985df717c9a69b39cfb1ea003
mariajosemarfantellez <mjmarfan@miuandes.cl>
2020-03-16 15:50:50 -0300

    Segundo Commit

--- ProyectoPrueba1/ProyectoPrueba1/Program.cs
+++ ProyectoPrueba1/ProyectoPrueba1/Program.cs
@@ -7,6 +7,7 @@ namespace ProyectoPrueba1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Segundo Commit");
         }
     }
 }
